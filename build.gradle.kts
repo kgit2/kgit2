d0710724fa838d7531f3e28e18b4a0b5da1d0eb7
BppleMan <1286363484@qq.com>
2022-08-21 02:24:54 +0800

    Temp

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,13 @@ kotlin {
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Mac OS X" -> macosArm64("native")
         hostOs == "Linux" -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-    
+
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -29,5 +29,31 @@ kotlin {
         }
         val nativeMain by getting
         val nativeTest by getting
+    }
+
+    nativeTarget.apply {
+        compilations.getByName("main") {
+            cinterops {
+                val libgit2 by creating {
+                    defFile(project.file("lib/libgit2.def"))
+                    packageName("libgit2")
+                }
+
+                val libcurl by creating {
+                    defFile(project.file("lib/libcurl.def"))
+                    packageName("libcurl")
+                }
+
+                val libdemo by creating {
+                    defFile(project.file("lib/libdemo.def"))
+                    packageName("libdemo")
+                }
+            }
+        }
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
     }
 }
---
+++ tools/build_libgit2.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+# build libgit2
+cd lib/libgit2 || exit
+mkdir build
+cd build
