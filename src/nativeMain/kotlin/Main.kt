ce0215710acc456ee0314bb19558753fc0aa3885
BppleMan <bppleman@gmail.com>
2022-11-30 02:15:35 +0800

    Test StableRef live-time

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,39 +1,10 @@
-import com.kgit2.KGit2
-import com.kgit2.config.Config
-import com.kgit2.utils.withTempDir
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import kotlinx.cinterop.StableRef
+import kotlinx.cinterop.asStableRef
 
 fun main(args: Array<String>) {
-    KGit2
-    withTempDir {
-        var config = openConfig(it, "foo")
-        runCatching {
-            config.getBool("foo.bar")
-        }.onFailure {
-            assert(true)
-        }.onSuccess {
-            assert(false)
-        }
-
-        config.setBool("foo.k1", true)
-        config.setInt32("foo.k2", 1)
-        config.setInt64("foo.k3", 2)
-        config.setString("foo.k4", "bar")
-        config.snapshot()
-        // config.free()
-
-        config = Config((it / "foo").toString()).snapshot()
-        assertTrue(config.getBool("foo.k1"))
-        assertEquals(1, config.getInt32("foo.k2"))
-        assertEquals(2L, config.getInt64("foo.k3"))
-        assertEquals("bar", config.getStringBuf("foo.k4"))
-
-        val entries = config.getEntries()
-        assertEquals(4, entries.list.size)
-
-        val snapshot = config.snapshot()
-        assertEquals("bar", snapshot.getString("foo.k4"))
-        // config.free()
-    }
+    val sb = StringBuilder("Hello, ")
+    val stableRef = StableRef.create(sb)
+    val ptr = stableRef.asCPointer()
+    sb.append("World!")
+    println(ptr.asStableRef<StringBuilder>().get().toString())
 }
