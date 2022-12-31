9b2e0d446dfcf6db3b2e129bba4d049f4203f8d8
BppleMan <bppleman@gmail.com>
2022-12-31 14:08:03 +0800

    Move out kbuilder

--- libs/linuxX64/toolchain.cmake
+++ libs/linuxX64/toolchain.cmake
@@ -4,6 +4,7 @@ set(CMAKE_C_COMPILER /root/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.
 
 set(CMAKE_CXX_COMPILER /root/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/bin/x86_64-unknown-linux-gnu-g++)
 
+# Semicolon-separated list of root paths to search on the filesystem.
 set(CMAKE_FIND_ROOT_PATH /root/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/x86_64-unknown-linux-gnu/sysroot)
 
 # adjust the default behavior of the FIND_XXX() commands:
